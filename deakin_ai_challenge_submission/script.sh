10d0d0e25728434a21dc614e0ec5b998c10d8c4f
Mohamed Reda Bouadjenek <rbouadjenek@gmail.com>
2022-03-17 16:57:22 +1100

    updates

--- deakin_ai_challenge_submission/script.sh
+++ deakin_ai_challenge_submission/script.sh
@@ -8,10 +8,10 @@ conda install numpy  2> /dev/null > /dev/null
 conda install tensorflow 2> /dev/null > /dev/null
 conda install keras 2> /dev/null > /dev/null
 conda install pillow 2> /dev/null > /dev/null
-
+conda install h5py 2> /dev/null > /dev/null
 echo $1
-ls -lh $1
+ls -lhR $1
 echo $2
-ls -lh $2
+ls -lhR $2
 
 python3 deakin_ai_challenge_submission.py $1 $2 
